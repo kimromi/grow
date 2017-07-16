c94655d9755ad1f42241f0661efd92f971afc793
kimromi <kimromi4@gmail.com>
2017-07-16 15:44:56 +0900

    fetch organizations and repositories

--- src/renderer/components/Main.vue
+++ src/renderer/components/Main.vue
@@ -4,6 +4,49 @@
 </template>
 
 <script>
+  import client from '../lib/github_client'
+  import db from '../lib/github_db'
+
+  export default {
+    async created () {
+      if (db.get(db.keys.orgs) === undefined) {
+        await this.fetchOrganizations()
+      }
+      if (db.get(db.keys.repos) === undefined) {
+        await this.fetchRepositories()
+      }
+      // let repo = await client.repositories()
+      // console.log(repo)
+      // client.pull_requests('muumuu-domain/muu.rb')
+      //   .then(function (response) {
+      //     console.log(response)
+      //   })
+    },
+    methods: {
+      async fetchOrganizations () {
+        let orgs = []
+        for (let org of await client.organizations()) {
+          orgs.push({
+            name: org.login,
+            managed: false
+          })
+        }
+        db.set(db.keys.orgs, orgs)
+      },
+      async fetchRepositories () {
+        let repos = []
+        for (let org of db.get(db.keys.orgs)) {
+          for (let repo of await client.repositories(org.name)) {
+            repos.push({
+              name: repo.name
+            })
+          }
+          break
+        }
+        db.set(db.keys.repos, repos)
+      }
+    }
+  }
 </script>
 
 <style lang='scss'>
--- src/renderer/lib/github_client.js
+++ src/renderer/lib/github_client.js
@@ -1,7 +1,47 @@
 import axios from 'axios'
+import config from './config'
 
 export default {
   check (url, token) {
     return axios.get(url, {headers: {'Authorization': `token ${token}`}})
+  },
+  http () {
+    return axios.create({
+      baseURL: config.get(config.keys.apiUrl),
+      timeout: 1000,
+      headers: {'Authorization': `token ${config.get(config.keys.token)}`}
+    })
+  },
+  async pageFetch (path) {
+    let page = 1
+    let returns = []
+    while (true) {
+      let fetched = []
+      await this.http().get(path, {params: {page: page}})
+        .then(function (response) {
+          if (response.data.length > 0) {
+            fetched = response.data
+            for (let repo of fetched) {
+              returns.push(repo)
+            }
+          }
+        })
+      if (fetched.length === 0) {
+        break
+      }
+      page++
+    }
+    return returns
+  },
+  async organizations () {
+    let orgs = await this.pageFetch('/user/orgs')
+    return orgs
+  },
+  async repositories (org) {
+    let repos = await this.pageFetch(`/orgs/${org}/repos`)
+    return repos
+  },
+  pull_requests (repo) {
+    return this.instance(`/repos/${repo}/pulls`).get()
   }
 }
