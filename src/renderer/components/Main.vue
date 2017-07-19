fff128ffef384dd7c5d82dd59ef37004a56ce432
kimromi <kimromi4@gmail.com>
2017-07-19 23:57:51 +0900

    delete waste

--- src/renderer/components/Main.vue
+++ src/renderer/components/Main.vue
@@ -4,7 +4,6 @@
 </template>
 
 <script>
-  import client from '../lib/github_client'
   import db from '../lib/github_db'
 
   export default {
@@ -15,27 +14,6 @@
       }
       if (db.get(db.keys.repos) === undefined || db.get(db.keys.repos) === null) {
         this.$router.push('repositories')
-      }
-
-      // let repo = await client.repositories()
-      // console.log(repo)
-      // client.pull_requests('muumuu-domain/muu.rb')
-      //   .then(function (response) {
-      //     console.log(response)
-      //   })
-    },
-    methods: {
-      async fetchRepositories () {
-        let repos = []
-        for (let org of db.get(db.keys.orgs)) {
-          for (let repo of await client.repositories(org.name)) {
-            repos.push({
-              name: repo.name
-            })
-          }
-          break
-        }
-        db.set(db.keys.repos, repos)
       }
     }
   }
